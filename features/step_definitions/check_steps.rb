02eaca41ee59f23a83aecb48182a16b6673f087a
Micah Geisel <originofstorms@gmail.com>
2009-10-04 15:33:53 -0700

    fix failing check_steps.feature.

--- features/step_definitions/check_steps.rb
+++ features/step_definitions/check_steps.rb
@@ -1,20 +1,20 @@
 Then /^I should see the current version of bard$/ do
   version = File.read("#{ROOT}/VERSION").chomp
-  @stdout.should include "bard     (#{version})"
+  @stdout.should =~ /bard\s+\(#{Regexp.escape(version)}\)/
 end
 
 Then /^I should see the current version of git$/ do
   version = `git --version`[/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/]
-  @stdout.should include "git      (#{version})"
+  @stdout.should =~ /git\s+\(#{Regexp.escape(version)}\)/
 end
 
 Then /^I should see the current version of rubygems$/ do
   version = `gem --version`.chomp
-  @stdout.should include "rubygems (#{version})"
+  @stdout.should =~ /rubygems\s+\(#{Regexp.escape(version)}\)/
 end
 
 Then /^I should see the current version of ruby$/ do
   version = `ruby --version`[/[0-9]+\.[0-9]+\.[0-9]+/]
-  @stdout.should include "ruby     (#{version})"
+  @stdout.should =~ /ruby\s+\(#{Regexp.escape(version)}\)/
 end
 
