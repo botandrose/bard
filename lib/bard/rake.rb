b2bc70a07654ca3e6bdae3f51af1bff1b850dcaa
Micah Geisel <micah@botandrose.com>
2010-02-20 14:41:16 -0800

    bootstrap task should load test schema.

--- lib/bard/rake.rb
+++ lib/bard/rake.rb
@@ -1,8 +1,8 @@
 task :bootstrap do
   `git submodule update --init` if `git submodule` =~ /^[^ ]/
   `cp config/database.sample.yml config/database.yml` unless File.exist?('config/database.yml')
-  `rake gems:install db:create RAILS_ENV=test`
-  `rake gems:install db:create RAILS_ENV=cucumber`
+  `rake gems:install db:create db:schema:load RAILS_ENV=test`
+  `rake gems:install RAILS_ENV=cucumber`
 end
 
 Rake::Task[:default].clear
