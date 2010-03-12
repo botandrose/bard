cc234cfbe4fe0dda9480781541e376d481367b4b
Micah Geisel <micah@botandrose.com>
2010-03-11 17:14:35 -0800

    added rake db:dump and db:load tasks.

--- lib/bard/rake.rb
+++ lib/bard/rake.rb
@@ -3,6 +3,31 @@ task :restart do
   system "touch tmp/debug.txt" if ENV["DEBUG"] == 'true'
 end
 
+namespace :db do
+  desc "Dump the current database to db/data.sql"
+  task :dump => :"backup:db" do
+    config = ActiveRecord::Base.configurations[RAILS_ENV || 'development']
+    db_file = Dir.glob("../#{config['database']}-*.sql").first
+    FileUtils.move db_file, "db/data.sql"
+  end
+
+  desc "Load the db/data.sql data into the current database."
+  task :load => :environment do
+    config = ActiveRecord::Base.configurations[RAILS_ENV || 'development']
+    Rake::Task["db:drop"].invoke
+    Rake::Task["db:create"].invoke
+    mysql = `which mysql`.strip
+    options  = " -u#{config['username']}"
+    options += " -p#{config['password']}" if config['password']
+    options += " -h #{config['host']}"    if config['host']
+
+    raise RuntimeError, "I only work with mysql." unless config['adapter'] == 'mysql'
+    raise RuntimeError, "Cannot find mysql." if mysql.blank?
+
+    sh "#{mysql} #{options} '#{config["database"]}' < db/data.sql"
+  end
+end
+
 desc "Bootstrap project"
 task :bootstrap => %w(bootstrap:files gems:install db:create db:migrate restart)
 
