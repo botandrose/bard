8ae3ceaaf090fb1ce768f2898f66f67eec8ca992
Micah Geisel <micah@botandrose.com>
2011-09-13 22:33:40 -0700

    compatibility with deploying to heroku cedar stack.

--- lib/bard.rb
+++ lib/bard.rb
@@ -99,7 +99,7 @@ class Bard < Thor
 
     if heroku?
       run_crucial "git push production", options.verbose?
-      run_crucial "heroku rake bootstrap:production:post", options.verbose?
+      run_crucial "heroku run rake bootstrap:production:post", options.verbose?
     else
       run_crucial "cap deploy", options.verbose?
     end
