3a5e4c0aa4555dd2a91480bf56220094eecbea42
Micah Geisel <micah@botandrose.com>
2023-03-01 12:38:33 -0600

    error out when asking for sudo password not in dev mode.

--- install_files/apt_dependencies.rb
+++ install_files/apt_dependencies.rb
@@ -1,10 +1,32 @@
 module AptDependencies
+  extend self
+
   def self.ensure!
-    deps = File.readlines("Aptfile", chomp: true)
-    deps.map do |dep|
-      "apt list #{dep} | grep '\\[installed\\]' || sudo apt install -y #{dep}"
-    end.join(" && ")
+    return "true" if deps_to_install.none?
+    if sudo_password_required? && ENV["RAILS_ENV"] != "development"
+      $stderr.puts "sudo requires password! cannot install #{deps_to_install.join}"
+      exit 1
+    else
+      "sudo apt install -y #{deps_to_install.join}"
+    end
+  end
+
+  private
+
+  def deps_to_install
+    installed_deps = `apt list imagemagick thunderbird  --installed 2>/dev/null`.chomp.split("\n")[1..]
+      .map { |line| line.split("/")[0] }
+    deps - installed_deps
+  end
+
+  def deps
+    @deps ||= File.readlines("Aptfile", chomp: true).select { |line| line.length > 0 }
   rescue Errno::ENOENT
-    "true"
+    @deps = []
+  end
+
+  def sudo_password_required?
+    !system("sudo -n true 2>/dev/null")
   end
 end
+
