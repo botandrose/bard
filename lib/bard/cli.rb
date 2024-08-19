0924f0f73f983a0dad5bd3501a6227bf3a241f62
Micah Geisel <micah@botandrose.com>
2024-08-19 12:09:18 +0200

    fix bard install.

--- lib/bard/cli.rb
+++ lib/bard/cli.rb
@@ -179,9 +179,9 @@ module Bard
 
     desc "install", "copies bin/setup and bin/ci scripts into current project."
     def install
-      install_files_path = File.expand_path(File.join(__dir__, "../install_files/*"))
+      install_files_path = File.expand_path(File.join(__dir__, "../../install_files/*"))
       system "cp -R #{install_files_path} bin/"
-      github_files_path = File.expand_path(File.join(__dir__, "../install_files/.github"))
+      github_files_path = File.expand_path(File.join(__dir__, "../../install_files/.github"))
       system "cp -R #{github_files_path} ./"
     end
 
