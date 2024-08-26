bf8b0362bb02666fbedb1230ce8d4455412c1b87
Micah Geisel <micah@botandrose.com>
2024-08-26 11:58:05 +0200

    allow provision to be run on existing projects without a provision url.

--- lib/bard/cli/provision.rb
+++ lib/bard/cli/provision.rb
@@ -4,8 +4,8 @@ module Bard::CLI::Provision
   def self.included mod
     mod.class_eval do
 
-      desc "provision [ssh_url]", "takes an ssh url to a raw ubuntu 22.04 install, and readies it in the shape of :production"
-      def provision ssh_url
+      desc "provision [ssh_url]", "takes an optional ssh url to a raw ubuntu 22.04 install, and readies it in the shape of :production"
+      def provision ssh_url=config[:production].ssh
         Bard::Provision.call(config, ssh_url.dup) # dup unfreezes the string for later mutation
       end
 
