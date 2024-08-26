7387c0c703fa34734a3b9a847a7d2e7267ad9da5
Micah Geisel <micah@botandrose.com>
2024-08-26 11:58:16 +0200

    restore provision data steps.

--- lib/bard/provision/data.rb
+++ lib/bard/provision/data.rb
@@ -4,14 +4,14 @@ class Bard::Provision::Data < Bard::Provision
   def call
     print "Data:"
 
-    # print " Dumping #{server.key} database to file"
-    # server.run! "bin/rake db:dump"
+    print " Dumping #{server.key} database to file"
+    server.run! "bin/rake db:dump"
 
-    # print " Transfering file from #{server.key},"
-    # server.copy_file "db/data.sql.gz", to: provision_server, verbose: false
+    print " Transfering file from #{server.key},"
+    server.copy_file "db/data.sql.gz", to: provision_server, verbose: false
 
-    # print " Loading file into database,"
-    # provision_server.run! "bin/rake db:load"
+    print " Loading file into database,"
+    provision_server.run! "bin/rake db:load"
 
     config.data.each do |path|
       print " Synchronizing files in #{path},"
