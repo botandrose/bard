01919672d366b66504c53feee46b216cbe559df5
Micah Geisel <micah@botandrose.com>
2025-10-24 16:55:19 -0600

    improve bard vim to include untracked files and ignore binaries and non-javascript asset files.

--- lib/bard/cli/vim.rb
+++ lib/bard/cli/vim.rb
@@ -4,7 +4,7 @@ module Bard::CLI::Vim
 
       desc "vim [branch=master]", "open all files that have changed since master"
       def vim branch="master"
-        exec "vim -p `git diff #{branch} --name-only | grep -v sass$ | tac`"
+        exec "vim -p `(git diff #{branch} --name-only; git ls-files --others --exclude-standard) | grep -v '^app/assets/images/' | grep -v '^app/assets/stylesheets/' | while read f; do [ -f \"$f\" ] && ! file -b \"$f\" | grep -q \"binary\" && echo \"$f\"; done | tac`"
       end
 
     end
--- spec/bard/cli/vim_spec.rb
+++ spec/bard/cli/vim_spec.rb
@@ -20,13 +20,13 @@ describe Bard::CLI::Vim do
     end
 
     it "should exec vim with git diff files by default" do
-      expect(cli).to receive(:exec).with("vim -p `git diff master --name-only | grep -v sass$ | tac`")
+      expect(cli).to receive(:exec).with("vim -p `(git diff master --name-only; git ls-files --others --exclude-standard) | grep -v '^app/assets/images/' | grep -v '^app/assets/stylesheets/' | while read f; do [ -f \"$f\" ] && ! file -b \"$f\" | grep -q \"binary\" && echo \"$f\"; done | tac`")
 
       cli.vim
     end
 
     it "should exec vim with specified branch" do
-      expect(cli).to receive(:exec).with("vim -p `git diff develop --name-only | grep -v sass$ | tac`")
+      expect(cli).to receive(:exec).with("vim -p `(git diff develop --name-only; git ls-files --others --exclude-standard) | grep -v '^app/assets/images/' | grep -v '^app/assets/stylesheets/' | while read f; do [ -f \"$f\" ] && ! file -b \"$f\" | grep -q \"binary\" && echo \"$f\"; done | tac`")
 
       cli.vim("develop")
     end
